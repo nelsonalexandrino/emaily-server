f1db8bd5e462cebcbb217ff943815862fd133d99
Nelson Alexandrino <nasson.alexander@gmail.com>
2018-06-01 01:43:05 +0200

    prod keys changes

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,10 @@
 //prod.js - production keys 
 
 module.exports = {
-    googleClientID: '92807794322-27rt2muvcrdoc3ijcn35315c1p66mi79.apps.googleusercontent.com',
-    googleClientSecret: 'oDvU0V-VN4WKQKB941NpQP9w',
-    mongoURI: 'mongodb://alexandrinoprod@ds143070.mlab.com:43070/emaily_prod',
-    username: 'alexandrinoprod',
-    password: 'historiasde#2018',
-    cookieKey: 'ldklkhdokhokokomooqoqmocmamcoamovomoamoasnvkonaconao'
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_USER_PASSWORD,
+    cookieKey: process.env.COOKIE_KEY
 };
