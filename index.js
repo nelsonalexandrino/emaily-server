c5a8fb6a525cd528ea43c18bafe7427a325c958f
Nelson Alexandrino <nasson.alexander@gmail.com>
2018-05-31 22:39:16 +0200

    teste

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('./services/passport');
 const keys = require('./config/keys');
-const PORT = process.env.PORT || 5000;
+require('./services/passport');
+require('./models/User');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
